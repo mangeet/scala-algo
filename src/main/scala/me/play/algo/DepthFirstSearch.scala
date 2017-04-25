f42cb85970b6cbb5365043ef266be2949eeec3e9
mangeetsingh_eden <mangeetsingh_eden@apple.com>
2017-04-24 20:14:43 -0700

    updated DFS

--- src/main/scala/me/play/algo/DepthFirstSearch.scala
+++ src/main/scala/me/play/algo/DepthFirstSearch.scala
@@ -1,5 +1,8 @@
 package me.play.algo
 
+import scala.collection.JavaConverters._
+import scala.collection.mutable.ListBuffer
+
 /**
   * Created by mangeeteden on 4/24/17.
   */
@@ -10,20 +13,22 @@ object DepthFirstSearch {
     // 1 -> 2 -> 3 -> 7
     // 1 -> 4 -> 6 -> 7
     // 2 -> 5 -> 6
+    // 8 -> 9
     val graph = new GraphBuilder[Int]().addEdge(1, 2).addEdge(2, 3).addEdge(3, 7).addEdge(1, 4).addEdge(4, 6).
-      addEdge(6, 7).addEdge(2, 5).addEdge(5, 6).build
+      addEdge(6, 7).addEdge(2, 5).addEdge(5, 6).addEdge(8, 9).build
 
-    val dfs = DepthFirstSearch[Int](graph, 1)
+    val dfs = DepthFirstSearch[Int](graph, 2)
 
-    println(s"Connected[1 -> 7] ?: $dfs.connected(7)")
-    println(s"Connected[1 -> 6] ?: $dfs.connected(6)")
+    println(dfs)
+    println(s"Connected[2 -> 6] ?: ${dfs.connected(6)}")
+    println(s"Connected[2 -> 8] ?: ${dfs.connected(8)}")
   }
 }
 
 case class DepthFirstSearch[T](graph: Graph[T], sourceVertex: T) {
 
   private[algo] var marked: Map[T, Boolean] = Map()
-  private[algo] var edgeTo: Map[T, T] = Map()
+  private[algo] var edgeTo: ListBuffer[(T, T)] = ListBuffer()
 
   {
     dfs(graph, sourceVertex)
@@ -32,16 +37,23 @@ case class DepthFirstSearch[T](graph: Graph[T], sourceVertex: T) {
   /* performs the dfs from source vertex 'v' */
   def dfs(graph: Graph[T], v: T): Unit = {
     marked += v -> true
-    for (w <- graph.adjTo(v)) {
-      if (!marked(w)) {
+    for(w <- graph.adjTo(v)) {
+      if (!isMarked(w)) {
         dfs(graph, w)
       }
-      edgeTo += v -> w
+      edgeTo += ((v, w))
+    }
+  }
+
+  private def isMarked(v: T): Boolean = {
+    marked.get(v) match {
+      case None => false
+      case _ => marked.get(v).get
     }
   }
 
   def connected(v: T) = {
-    marked(v)
+    isMarked(v)
   }
 
   override def toString(): String = {
